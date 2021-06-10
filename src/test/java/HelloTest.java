c912c034640fb9c40b4633a57ba0c9969151aa25
sumeyse <sumeyse@gmail.com>
2021-06-10 20:14:25 +0300

    work1

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,7 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello Test");
         System.out.println("About to share project on GitHub");
+
+        //work1
     }
 }
